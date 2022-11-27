6dc59b263c1ce4c63fa51a96039cee6231c04d74
Destiny Rogers <destinyrogers725@gmail.com>
2022-11-27 14:59:01 -0800

    adds infobox and title to wiki page

---
+++ wiki-me/src/Article.js
@@ -0,0 +1,15 @@
+import Infobox from "./Infobox.js";
+import './App.css';
+function Article() {
+  return (
+    <div style={{margin: "-1px 0px 0px 176px", padding: "20px 24px 24px"}}>
+        <h1 className="firstHeading mw-first-heading">Destiny Rogers</h1>
+      <Infobox />
+      <p>
+        <b>Destiny Rogers</b> (born 8 May 1998) is a San Francisco based software engineer, animal lover and 
+      </p>
+    </div>
+  );
+}
+
+export default Article;
--- wiki-me/src/Infobox.js
+++ wiki-me/src/Infobox.js
@@ -1,7 +1,60 @@
-function Infobox(){
-    return(
-        <div>
-        Infobox
-        </div>
-    )
+import dedny from "./dedny_nov24.jpeg";
+import "./App.css";
+
+function Infobox() {
+  return (
+    <div className="infobox">
+      <div
+        style={{
+          backgroundColor: "#b0c4de",
+          fontSize: "125%",
+          textAlign: "center",
+          fontWeight: "bolder",
+        }}
+      >
+        Destiny Rogers
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <img src={dedny} />
+      </div>
+      Destiny sitting at home with Cat behind her in 2022
+      <div
+        style={{
+          backgroundColor: "#b0c4de",
+          fontSize: "125%",
+          textAlign: "center",
+          fontWeight: "bolder",
+        }}
+      >
+        Background Information
+      </div>
+      <table>
+        <tr>
+          <td style={{ fontWeight: "bold" }}>Birth name</td>
+          <td>Destiny Rogers</td>
+        </tr>
+        <tr>
+          <td style={{ fontWeight: "bold" }}>Born</td>
+          <td>
+            May 8, 1998 {"("}age 24{")"}
+          </td>
+        </tr>
+        <tr>
+          <td style={{ fontWeight: "bold" }}>Occupation(s)</td>
+          <td>
+            Software engineer • Quality Engineer • Frontend Developer • Full
+            Stack Engineer
+          </td>
+        </tr>
+        <tr>
+          <td style={{ fontWeight: "bold" }}>Hobbies and Interests</td>
+          <td>
+            Record shopping • bass • hiking • 3d printing • movies • video games
+            • painting
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 }
+export default Infobox;
